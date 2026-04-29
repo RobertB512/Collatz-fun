3f320eef7d81db85a93eedc4334ec699d1a15d28
RobertB512 <rwbcool@gmail.com>
2026-04-28 18:07:18 -0700

    Move small functions out of main function in collatz generation file. Add output data.

--- collatz.js
+++ collatz.js
@@ -3,102 +3,80 @@
 const fs = require("fs");
 const { json } = require("stream/consumers");
 
-const generateCollatzSequence = async () => {
-  const writeToFile = async (path, data) => {
-    try {
-      await fs.promises.appendFile(path, data)
-    } catch (err) {
-      console.log(`Error writing to file: ${err}.`);
-    }
-  }
-
-  const findPrevSeed = async (path) => {
-    const data = (await fs.promises.readFile(path, "utf8")).trim();
-    nextSeed = null;
-    if (data) {
-      const lastLine = data.slice(data.lastIndexOf("\n") + 1);
-			const lastObj = JSON.parse(lastLine);
-			nextSeed = Number(lastObj.seed);
-    } 
-    
-    return nextSeed;
-  }
-  
-  const outputFile = "collatzOutput.ndjson";
-  let startingSeed = await findPrevSeed(outputFile) + 1; // Starting number for Collatz sequence.
-
-  if (startingSeed == null) {
-    startingSeed = 1;
-  }
-  
-  const numbersToAdd = 5;                         // The number of Collatz sequences to add to the output file.
-  const endingSeed   = startingSeed + numbersToAdd; // Ending number for Collatz sequence.
-
-  console.log(`Next value is: ${startingSeed} \n`);
-  console.log("Processing... Please wait");
-  console.log("If you stop this process, data may be lost");
-
-  while (startingSeed <= endingSeed) {
-    let highestHailstone = 0;            // The highest number in any given Collatz sequence.
-    let valueOfN         = startingSeed; // Number being processed toward the 4-2-1 loop.
-    let stepsToReachLoop = 0;            // Number of steps to reach 1.
-    let sequenceValues   = [];           // List of values produced from startingSeed to 1.
-
-    do {
-      stepsToReachLoop++;
-
-			if (valueOfN % 2 === 0) {
-				valueOfN = valueOfN / 2;
-			} else if (valueOfN % 2 === 1) {
-				valueOfN = valueOfN * 3 + 1;
-			}
-
-			if (valueOfN > highestHailstone) highestHailstone = valueOfN;
-
-			// sequenceValues.push(valueOfN.toLocaleString());
-      sequenceValues.push(valueOfN);
-
-    } while (valueOfN >= 2);
-
-    // while (valueOfN >= 2) {
-    //   stepsToReachLoop++;
-
-    //   if (valueOfN % 2 === 0) {
-    //     valueOfN = valueOfN / 2;
-    //   } else if (valueOfN % 2 === 1) {
-    //     valueOfN = valueOfN * 3 + 1;
-    //   }
-
-    //   if (valueOfN > highestHailstone) highestHailstone = valueOfN;
-
-    //   sequenceValues.push(valueOfN.toLocaleString());
-    // }
-    
-    // const outputDivider       = "\n\n------------------------------";
-    // const startValueReport       = {originalN: startingSeed.toLocaleString()}; 
-    // const sequenceValuesReport   = {sequenceValues: sequenceValues}; 
-    // const stepCountReport        = {stepCount: stepsToReachLoop.toLocaleString()};
-    // const highestHailstoneReport = {highestHailstone: highestHailstone.toLocaleString()};
-
-    const fullSequenceReport = {
-			seed: startingSeed,
-			hailstoneSeq: sequenceValues,
-			stepCount: stepsToReachLoop,
-			highestHailstone: highestHailstone,
-
-			// outputDivider,
-			// startValueReport,
-			// sequenceValuesReport,
-			// stepCountReport,
-			// highestHailstoneReport
-		};
-    
-    await writeToFile(outputFile, JSON.stringify(fullSequenceReport) + "\n");
-
-    startingSeed++;
-  }
-
-  console.log("\nFinished writing to file!");
+const writeToFile = async (path, data) => {
+	try {
+		await fs.promises.appendFile(path, data);
+	} catch (err) {
+		console.log(`Error writing to file: ${err}.`);
+	}
 };
 
-generateCollatzSequence();
+const findPrevSeed = async (path) => {
+	const data = (await fs.promises.readFile(path, "utf8")).trim();
+	let nextSeed = null;
+
+	if (data) {
+		const lastLine = data.slice(data.lastIndexOf("\n") + 1);
+		const lastObj = JSON.parse(lastLine);
+		nextSeed = Number(lastObj.seed);
+	}
+
+	return nextSeed;
+};
+
+const generateCollatzSeq = async (startSeed, endSeed, file) => {
+  while (startSeed <= endSeed) {
+		let highestHailstone = 0; // The highest number in any given Collatz sequence.
+		let valOfN = startSeed; // Number being processed toward the 4-2-1 loop.
+		let steps = 0; // Number of steps to reach 1.
+		let seqVals = []; // List of values produced from startingSeed to 1.
+
+		do {
+			steps++;
+
+			if (valOfN % 2 === 0) {
+				valOfN = valOfN / 2;
+			} else if (valOfN % 2 === 1) {
+				valOfN = valOfN * 3 + 1;
+			}
+
+			if (valOfN > highestHailstone) highestHailstone = valOfN;
+
+			seqVals.push(valOfN);
+		} while (valOfN >= 2);
+
+		const fullSeqReport = {
+			seed: startSeed,
+			hailstoneSeq: seqVals,
+			stepCount: steps,
+			highestHailstone: highestHailstone,
+		};
+
+		await writeToFile(file, JSON.stringify(fullSeqReport) + "\n");
+
+		startSeed++;
+	}
+
+	console.log("\nFinished writing to file!");
+};
+
+const prepSeqGeneration = async () => {
+	const outputFile = "collatzOutput.ndjson";
+  let startingSeed = (await findPrevSeed(outputFile)) + 1; // Starting number for Collatz sequence.
+
+	if (startingSeed == null) {
+		startingSeed = 1;
+	}
+
+	const numbersToAdd = 5; // The number of Collatz sequences to add to the output file.
+	const endingSeed = startingSeed + numbersToAdd; // Ending number for Collatz sequence.
+
+	console.log(`Next value is: ${startingSeed} \n`);
+  console.log(`Adding ${numbersToAdd} more numbers...`);
+	console.log("Processing... Please wait");
+	console.log("If you stop this process, data may be lost or messed up");
+
+	generateCollatzSeq(startingSeed, endingSeed, outputFile);
+};
+
+prepSeqGeneration();
