8257b111f2ecf54b3ac390ff3e6a69be79c3870d
RobertB512 <rwbcool@gmail.com>
2026-04-30 11:16:47 -0700

    Add package.json file and log contents of ndjson output file line by line

--- analysisFunctions/getFileContents.js
+++ analysisFunctions/getFileContents.js
@@ -1,9 +1,35 @@
 import fs from "node:fs";
-import { readFile } from "node:fs";
+import readline from "node:readline";
 
 export const getFileContents = async (path) => {
-	const data = await fs.promises.readFile(path, "utf8");
-	const sections = data.split("------------------------------");
-	const collatzSeq = sections;
-	return collatzSeq;
+	// const data = await fs.promises.readFile(path, "utf8");
+	// const sections = data.split("------------------------------");
+	// const collatzSeq = sections;
+	// return collatzSeq;
+
+	const stream = fs.createReadStream(path);
+
+	const rl = readline.createInterface({
+		input: stream,
+		crlfDelay: Infinity,
+	});
+
+	// const state = {
+	// 	lastSeed: null,
+	// 	// anything else shared across functions
+	// };
+
+	for await (const line of rl) {
+		if (!line.trim()) continue;
+
+		const obj = JSON.parse(line);
+
+		console.log(obj);
+
+		// processObject(obj, state);
+
+		// state.lastSeed = obj.seed;
+
+		// console.log(state);
+	}
 };
--- outputAnalysis.js
+++ outputAnalysis.js
@@ -18,67 +18,67 @@ import { formatResults } from "./analysisFunctions/formatResults.js";
 const analyzeCollatzOutput = async () => {
 	console.time("timeProg");
 
-	const sourceFile = "collatzOutput.txt";
+	const sourceFile = "collatzOutput.ndjson";
 	const outputFile = "outputAnalysis.txt";
-	const collatzData = await getFileContents(sourceFile);
-	const seedInfo = collatzData.splice(2, collatzData.length - 1); // To remove some extra text
-	const brokenUpSeq = breakUpSeq(seedInfo);
-	const firstAndLastSeed = findFirstAndLastSeed(brokenUpSeq);
-	const seqLengthStats = analyzeHailstoneSeq(brokenUpSeq);
-	const largestHailstone = findLargestHailstone(brokenUpSeq);
-	const longestStraightDrop = findLongestStraightDrop(brokenUpSeq);
-	const firstToBreak100 = findFirstToBreakX(brokenUpSeq, 100);
-	const firstToBreak500 = findFirstToBreakX(brokenUpSeq, 500);
-	const firstToBreak1Th = findFirstToBreakX(brokenUpSeq, 1_000);
-	const firstToBreak5Th = findFirstToBreakX(brokenUpSeq, 5_000);
-	const firstToBreak10Th = findFirstToBreakX(brokenUpSeq, 10_000);
-	const firstToBreak25Th = findFirstToBreakX(brokenUpSeq, 25_000);
-	const firstToBreak50Th = findFirstToBreakX(brokenUpSeq, 50_000);
-	const firstToBreak100Th = findFirstToBreakX(brokenUpSeq, 100_000);
-	const firstToBreak500Th = findFirstToBreakX(brokenUpSeq, 500_000);
-	const firstToBreak1M = findFirstToBreakX(brokenUpSeq, 1_000_000);
-	const firstToBreak5M = findFirstToBreakX(brokenUpSeq, 5_000_000);
-	const firstToBreak10M = findFirstToBreakX(brokenUpSeq, 10_000_000);
-	const firstToBreak25M = findFirstToBreakX(brokenUpSeq, 25_000_000);
-	const firstToBreak50M = findFirstToBreakX(brokenUpSeq, 50_000_000);
-	const firstToBreak100M = findFirstToBreakX(brokenUpSeq, 100_000_000);
-	const firstToBreak500M = findFirstToBreakX(brokenUpSeq, 500_000_000);
-	const firstToBreak1B = findFirstToBreakX(brokenUpSeq, 1_000_000_000);
+	await getFileContents(sourceFile);
+	// const seedInfo = collatzData.splice(2, collatzData.length - 1); // To remove some extra text
+	// const brokenUpSeq = breakUpSeq(seedInfo);
+	// const firstAndLastSeed = findFirstAndLastSeed(brokenUpSeq);
+	// const seqLengthStats = analyzeHailstoneSeq(brokenUpSeq);
+	// const largestHailstone = findLargestHailstone(brokenUpSeq);
+	// const longestStraightDrop = findLongestStraightDrop(brokenUpSeq);
+	// const firstToBreak100 = findFirstToBreakX(brokenUpSeq, 100);
+	// const firstToBreak500 = findFirstToBreakX(brokenUpSeq, 500);
+	// const firstToBreak1Th = findFirstToBreakX(brokenUpSeq, 1_000);
+	// const firstToBreak5Th = findFirstToBreakX(brokenUpSeq, 5_000);
+	// const firstToBreak10Th = findFirstToBreakX(brokenUpSeq, 10_000);
+	// const firstToBreak25Th = findFirstToBreakX(brokenUpSeq, 25_000);
+	// const firstToBreak50Th = findFirstToBreakX(brokenUpSeq, 50_000);
+	// const firstToBreak100Th = findFirstToBreakX(brokenUpSeq, 100_000);
+	// const firstToBreak500Th = findFirstToBreakX(brokenUpSeq, 500_000);
+	// const firstToBreak1M = findFirstToBreakX(brokenUpSeq, 1_000_000);
+	// const firstToBreak5M = findFirstToBreakX(brokenUpSeq, 5_000_000);
+	// const firstToBreak10M = findFirstToBreakX(brokenUpSeq, 10_000_000);
+	// const firstToBreak25M = findFirstToBreakX(brokenUpSeq, 25_000_000);
+	// const firstToBreak50M = findFirstToBreakX(brokenUpSeq, 50_000_000);
+	// const firstToBreak100M = findFirstToBreakX(brokenUpSeq, 100_000_000);
+	// const firstToBreak500M = findFirstToBreakX(brokenUpSeq, 500_000_000);
+	// const firstToBreak1B = findFirstToBreakX(brokenUpSeq, 1_000_000_000);
 
-	const resultsArray = [
-		firstAndLastSeed,
-		seqLengthStats,
-		longestStraightDrop,
-		firstToBreak100,
-		firstToBreak500,
-		firstToBreak1Th,
-		firstToBreak5Th,
-		firstToBreak10Th,
-		firstToBreak25Th,
-		firstToBreak50Th,
-		firstToBreak100Th,
-		firstToBreak500Th,
-		firstToBreak1M,
-		firstToBreak5M,
-		firstToBreak10M,
-		firstToBreak25M,
-		firstToBreak50M,
-		firstToBreak100M,
-		largestHailstone,
-		firstToBreak500M,
-		firstToBreak1B,
-	];
+	// const resultsArray = [
+	// 	firstAndLastSeed,
+	// 	seqLengthStats,
+	// 	longestStraightDrop,
+	// 	firstToBreak100,
+	// 	firstToBreak500,
+	// 	firstToBreak1Th,
+	// 	firstToBreak5Th,
+	// 	firstToBreak10Th,
+	// 	firstToBreak25Th,
+	// 	firstToBreak50Th,
+	// 	firstToBreak100Th,
+	// 	firstToBreak500Th,
+	// 	firstToBreak1M,
+	// 	firstToBreak5M,
+	// 	firstToBreak10M,
+	// 	firstToBreak25M,
+	// 	firstToBreak50M,
+	// 	firstToBreak100M,
+	// 	largestHailstone,
+	// 	firstToBreak500M,
+	// 	firstToBreak1B,
+	// ];
 
-	const formatedResults = formatResults(resultsArray);
+	// const formatedResults = formatResults(resultsArray);
 
-	try {
-		await writeFile(outputFile, formatedResults);
-		console.log("File written successfully");
-	} catch (err) {
-		console.error("Error writing file:", err);
-	}
+	// try {
+	// 	await writeFile(outputFile, formatedResults);
+	// 	console.log("File written successfully");
+	// } catch (err) {
+	// 	console.error("Error writing file:", err);
+	// }
 
-  console.timeEnd("timeProg");
+  // console.timeEnd("timeProg");
 
 };
 
